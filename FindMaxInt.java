8c88cf3393162400b4a0ccdef789ad69328d4073
Angelica <Angelica137@users.noreply.github.com>
2021-04-03 16:33:53 +0100

    update pseudo code to remove obsolete

--- FindMaxInt.java
+++ FindMaxInt.java
@@ -16,8 +16,7 @@ public class FindMaxInt {
 		// 2. for i = 2 to A.length
 		// 3. if A[i] > max
 		// 4. max = A[i]
-		// 5. i = i + 1
-		// 6. return max
+		// 5. return max
 		int i = 0;
 		int max = array[i];
 		for (i = 1; i < array.length; i++) {
